8929d48fcef29d1bf867feeedf66669ef2a0ee96
ivanshchitov <amoral-time@bk.ru>
2013-11-22 20:31:43 +0400

    Add empty Main class.

---
+++ src/com/github/ivanshchitov/simplegraphicaleditor/Main.java
@@ -0,0 +1,18 @@
+package com.github.ivanshchitov.simplegraphicaleditor;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ivan Shchitov. <polotenchik1992@gmail.com>
+ * Date: 22.11.13
+ * Time: 20:24
+ */
+public class Main {
+
+    /**
+     * Entry point in the program.
+     * @param args command-line arguments
+     */
+    public static void main(String[] args){
+
+    }
+}
