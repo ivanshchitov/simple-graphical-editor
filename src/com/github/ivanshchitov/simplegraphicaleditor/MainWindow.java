357d9eb938a5390684228138314cb971334a3388
ivanshchitov <amoral-time@bk.ru>
2013-11-24 23:55:32 +0400

    Add toolbars on MainWindow.

--- src/com/github/ivanshchitov/simplegraphicaleditor/MainWindow.java
+++ src/com/github/ivanshchitov/simplegraphicaleditor/MainWindow.java
@@ -1,6 +1,14 @@
 package com.github.ivanshchitov.simplegraphicaleditor;
 
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JMenuBar;
+import javax.swing.JToolBar;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.Color;
+import java.awt.BorderLayout;
 
 /**
  * Class for display main window application.
@@ -15,8 +23,12 @@ public class MainWindow extends JFrame {
      */
     public MainWindow() {
         super("Simple Graphical Editor");
+        setSize(500, 500);
         initMenus();
-        setBounds(0, 0, 500, 500);
+        initShapeToolBar();
+        initColorToolBar();
+        setLayout(new BorderLayout());
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
     }
 
@@ -36,5 +48,49 @@ public class MainWindow extends JFrame {
         menuBar.add(fileMenu);
         menuBar.add(aboutMenu);
         setJMenuBar(menuBar);
+    }
+
+    /**
+     * Initializes toolbar for choose shape.
+     */
+    private void initShapeToolBar() {
+        JToolBar toolBar = new JToolBar("Toolbar", JToolBar.VERTICAL);
+        toolBar.add(new JButton(new ImageIcon("res/rect.png")));
+        toolBar.add(new JButton(new ImageIcon("res/circle.png")));
+        toolBar.add(new JButton(new ImageIcon("res/line.png")));
+        toolBar.setBounds(0, 0, 40, this.getHeight());
+        add(toolBar);
+    }
+
+    /**
+     * Initializes toolbar fo choose color.
+     */
+    private void initColorToolBar() {
+        JToolBar jtoolBar = new JToolBar("Colorbar", JToolBar.HORIZONTAL);
+        jtoolBar.add(createButton(Color.red, 15, 5, 20, 20));
+        jtoolBar.add(createButton(Color.blue, 45, 5, 20, 20));
+        jtoolBar.add(createButton(Color.yellow, 75, 5, 20, 20));
+        jtoolBar.add(createButton(Color.green, 105, 5, 20, 20));
+        jtoolBar.add(createButton(Color.black, 135, 5, 20, 20));
+        jtoolBar.add(createButton(Color.white, 165, 5, 20, 20));
+        jtoolBar.setLayout(null);
+        jtoolBar.setBounds(40, 0, this.getWidth(), 30);
+        add(jtoolBar);
+    }
+
+    /**
+     * Creates button with different color, placement and sizes.
+     * @param color the background color
+     * @param x  the new x-coordinate of this component
+     * @param y   the new y-coordinate of this component
+     * @param width  the new width of this component
+     * @param height  the new height of this component
+     * @return new button
+     */
+    private JButton createButton(Color color, int x, int y, int width, int height) {
+        JButton button = new JButton();
+        button.setBackground(color);
+        button.setBounds(x, y, width, height);
+        return button;
     }
 }
