29e550ef7c537cb4baf25016c8d9a4e859ea12a7
ivanshchitov <amoral-time@bk.ru>
2013-11-24 19:02:05 +0400

    Add realization of empty window.

---
+++ src/com/github/ivanshchitov/simplegraphicaleditor/MainWindow.java
@@ -0,0 +1,17 @@
+package com.github.ivanshchitov.simplegraphicaleditor;
+
+import javax.swing.JFrame;
+
+/**
+ * Class for display main window application.
+ * User: Ivan Shchitov. <polotenchik1992@gmail.com>
+ * Date: 24.11.13
+ * Time: 18:58
+ */
+public class MainWindow extends JFrame {
+
+    public MainWindow() {
+        super("Simple Graphical Editor");
+        setBounds(0, 0, 500, 500);
+    }
+}
